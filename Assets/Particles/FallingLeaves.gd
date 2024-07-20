35f73ed8321e5aa1d8d7bf380ccf3e7ad4cee8c3
Lewis Sharpe <lewis.sharpe12@googlemail.com>
2024-07-20 12:54:45 +0100

    Audio and leaves adjustments

--- Assets/Particles/FallingLeaves.gd
+++ Assets/Particles/FallingLeaves.gd
@@ -1,7 +1,10 @@
 extends GPUParticles3D
 
-@export var wind_force := 25;
-@export var down_wind_force := 50;
+@export var wind_force_multiplier := 5;
+@export var max_wind_force := 25;
+var current_wind_force := 25;
+
+@export var down_wind_force := 5;
 @export var petal_gravity := -5;
 
 # Called when the node enters the scene tree for the first time.
@@ -12,11 +15,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Left"):
-		process_material.set("gravity", Vector3(wind_force, petal_gravity, 0))
+		process_material.set("gravity", Vector3(current_wind_force, petal_gravity, -down_wind_force))
 	if Input.is_action_pressed("Right"):
-		process_material.set("gravity", Vector3(-wind_force, petal_gravity, 0))
+		process_material.set("gravity", Vector3(-current_wind_force, petal_gravity, -down_wind_force))
 	if Input.is_action_just_released("Left") or Input.is_action_just_released("Right"):
-		process_material.set("gravity", Vector3(0, petal_gravity, 0))
-	
+		process_material.set("gravity", Vector3(0, petal_gravity, -down_wind_force))
 	
 
--- main.gd
+++ main.gd
@@ -14,7 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#if bottom_area.has_overlapping_bodies():
-		#print(bottom_area.get_overlapping_bodies())
-		#boat.global_position = boat_spawn.global_position
+	if bottom_area.has_overlapping_bodies():
+		if bottom_area.get_overlapping_bodies():
+			boat.global_position = boat_spawn.global_position
 	pass
