db288fbfdbd68dcf474e871e1fab7b363c50ac87
Legominds <OfficialLegominds@users.noreply.github.com>
2024-07-20 19:53:14 +0100

    Collisions and co

--- main.gd
+++ main.gd
@@ -2,18 +2,8 @@ extends Node3D
 
 var boat_spawn
 var boat
-var bottom_area
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	boat_spawn = $BoatSpawn
 	boat = $Boat
-	bottom_area = $RespawnArea
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if bottom_area.has_overlapping_bodies():
-		if bottom_area.get_overlapping_bodies():
-			boat.global_position = boat_spawn.global_position
-	pass
