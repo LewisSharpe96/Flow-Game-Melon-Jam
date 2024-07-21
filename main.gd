55cd02ae3ba872e89e1ac3dc5ce40e7bafc59473
Lewis Sharpe <lewis.sharpe12@googlemail.com>
2024-07-21 14:12:05 +0100

    Respawn on collision

--- main.gd
+++ main.gd
@@ -2,11 +2,17 @@ extends Node3D
 
 var boat_spawn
 var boat
+var original_boat_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	boat_spawn = $BoatSpawn
 	boat = $Boat
+	original_boat_position = boat.global_position
+	
+func _process(delta):
+	
+	pass
 
 func _on_game_end_body_entered(body):
 	print("You Win")
